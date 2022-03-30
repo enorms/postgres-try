01c2409e156ad6debafdcb0c5e679490efa65576
Eric Norman <eric@norman.zone>
2022-03-29 19:59:12 -0700

    Sign

--- postgrestry/views.py
+++ postgrestry/views.py
@@ -7,9 +7,7 @@ from .models import Sign
 
 # Create your views here.
 def index(request):
-    r = requests.get('https://httpbin.org/status/418')
-    print(r.text)
-    return HttpResponse('<pre>' + r.text + '</pre>')
+    return print("views > index()")
 
 def db(request):
 
@@ -19,3 +17,4 @@ def db(request):
     sign = Sign.objects.all()
 
     return render(request, "db.html", {"sign": sign})
+    
